f077b029ce10fd4a24182b8b91a5f8979e52e3e3
Andrey Ziuzin <zyzin@hotmail.com>
2020-12-09 17:28:55 +0300

    search section is done

--- js/main.js
+++ js/main.js
@@ -6,4 +6,14 @@ $(function () {
     nextArrow:
       '<button class="banner__slider-btn banner__slider-btn--next"><img src="img/arrow-right.svg" alt=""></button>',
   });
+
+  $('.search__tab').on('click', function (e) {
+    e.preventDefault();
+
+    $('.search__tab').removeClass('search__tab--active');
+    $('.search__content-item').removeClass('search__content-item--active');
+
+    $(this).addClass('search__tab--active');
+    $($(this).attr('href')).addClass('search__content-item--active');
+  });
 });
---
+++ js/my.js
@@ -0,0 +1,2 @@
+const tab = document.querySelector('.search__tab');
+const tabContent = document.querySelector('.search__content-item ');
