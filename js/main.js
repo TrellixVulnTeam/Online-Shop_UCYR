37c7354b93770330a97efc37b0069721131bd928
Andrey Ziuzin <zyzin@hotmail.com>
2020-12-09 17:28:19 +0300

    banner section is done/ fix dots

--- js/main.js
+++ js/main.js
@@ -1,30 +1,9 @@
-const openMobMenu = document.querySelector('.header__mob-nav');
-const mobMenu = document.querySelector('.header__mob-menu');
-const mobManuActive = document.querySelector('.header__mob-menu--active');
-
-openMobMenu.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  mobMenu.classList.add('header__mob-menu--active');
+$(function () {
+  $('.banner__slider').slick({
+    dots: true,
+    prevArrow:
+      '<button class="banner__slider-btn banner__slider-btn--prev"><img src="img/arrow-left.svg" alt=""></button>',
+    nextArrow:
+      '<button class="banner__slider-btn banner__slider-btn--next"><img src="img/arrow-right.svg" alt=""></button>',
+  });
 });
-
-// const btnMenu = document.querySelector('.btn');
-// const menu = document.querySelector('.menu');
-// const toggleMenu = function() {
-//     menu.classList.toggle('open');
-// }
-
-// btnMenu.addEventListener('click', function(e) {
-//     e.stopPropagation();
-//     toggleMenu();
-// });
-
-// document.addEventListener('click', function(e) {
-//     const target = e.target;
-//     const its_menu = target == menu || menu.contains(target);
-//     const its_btnMenu = target == btnMenu;
-//     const menu_is_active = menu.classList.contains('open');
-
-//     if (!its_menu && !its_btnMenu && menu_is_active) {
-//         toggleMenu();
-//     }
-// });
