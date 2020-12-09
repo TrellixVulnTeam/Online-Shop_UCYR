3a5491dc8516e43902c1feb4bf4e55aed59ee5c8
Andrey Ziuzin <zyzin@hotmail.com>
2020-12-09 22:20:35 +0300

    Slider of Popular is done

--- js/main.js
+++ js/main.js
@@ -16,4 +16,25 @@ $(function () {
     $(this).addClass('search__tab--active');
     $($(this).attr('href')).addClass('search__content-item--active');
   });
+
+  $('.popular__tab').on('click', function (e) {
+    e.preventDefault();
+
+    $('.popular__tab').removeClass('popular__tab--active');
+    $('.popular__content-item').removeClass('popular__content-item--active');
+
+    $(this).addClass('popular__tab--active');
+    $($(this).attr('href')).addClass('popular__content-item--active');
+  });
+
+  $('.popular__list').slick({
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    prevArrow:
+      '<button class="popular__slider-btn popular__slider-btn--prev"><img src="img/icons/arrow-left-dark.png" alt=""></button>',
+    nextArrow:
+      '<button class="popular__slider-btn popular__slider-btn--next"><img src="img/icons/arrow-right-dark.png" alt=""></button>',
+  });
 });
